a2e1682585cde53f8070304816c7df0d97b460a7
Artem Kleinschmidt <artem.kleinschmidt@gmail.com>
2022-08-22 16:34:54 +0300

    added basic weight detail card

--- app_packages/daily_presentation/lib/daily_presentation.dart
+++ app_packages/daily_presentation/lib/daily_presentation.dart
@@ -1,4 +1,3 @@
 library daily_presentation;
 
 export 'src/daily_screen/daily_screen.dart';
-export 'src/daily_screen/weight_bloc.dart' show WeightBloc;
--- app_packages/daily_presentation/lib/src/daily_screen/daily_screen.dart
+++ app_packages/daily_presentation/lib/src/daily_screen/daily_screen.dart
@@ -1,4 +1,5 @@
 import 'package:common_presentation/common_presentation.dart';
+import 'package:daily_presentation/src/daily_screen/WeightDetailsCard.dart';
 import 'package:daily_presentation/src/daily_screen/weight_card.dart';
 import 'package:di/di.dart';
 import 'package:flutter/material.dart';
@@ -8,17 +9,24 @@ import 'package:carousel_slider/carousel_slider.dart';
 
 import 'weight_bloc.dart';
 
-class DailyScreen extends StatelessWidget implements FabClickListener {
+class DailyScreen extends StatefulWidget implements FabClickListener {
   DailyScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return _dailyScreenContent(context);
-  }
+  State<DailyScreen> createState() => _DailyScreenState();
 
   @override
   void onFabClicked() {
     debugPrint("Fab clicked");
+  }
+}
+
+class _DailyScreenState extends State<DailyScreen> {
+  var _selectedWeightIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return _dailyScreenContent(context);
   }
 
   Widget _dailyScreenContent(BuildContext context) {
@@ -29,30 +37,60 @@ class DailyScreen extends StatelessWidget implements FabClickListener {
       child: BlocBuilder<WeightBloc, WeightState>(builder: (context, state) {
         debugPrint("DailyScreen state $state");
 
+        if (state is WeightLoading) {
+          return const Center(
+            child: SizedBox(
+              width: 200,
+              height: 200,
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
         if (state is! WeightSuccess) return Container();
 
         List<Weight> weightList = state.weightList;
 
-        return CarouselSlider(
-          options: CarouselOptions(
-            height: 200.0,
-            enlargeCenterPage: true,
-            viewportFraction: 0.6,
-            enableInfiniteScroll: false,
-            reverse: true,
-          ),
-          items: weightList.map((weight) {
-            return Builder(
-              builder: (BuildContext context) {
-                return Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: WeightCard(weight),
+        Weight selectedWeight;
+        if (_selectedWeightIndex == -1) {
+          selectedWeight = state.weightList.first;
+        } else {
+          selectedWeight = weightList[_selectedWeightIndex];
+        }
+
+        return Column(
+          children: [
+            CarouselSlider(
+              options: CarouselOptions(
+                  height: 200.0,
+                  enlargeCenterPage: true,
+                  viewportFraction: 0.6,
+                  enableInfiniteScroll: false,
+                  reverse: true,
+                  onPageChanged:
+                      (int index, CarouselPageChangedReason reason) =>
+                          onPageChanged(index)),
+              items: weightList.map((weight) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: WeightCard(weight),
+                    );
+                  },
                 );
-              },
-            );
-          }).toList(),
+              }).toList(),
+            ),
+            WeightDetailsCard(selectedWeight)
+          ],
         );
       }),
     );
+  }
+
+  onPageChanged(int index) {
+    setState(() {
+      _selectedWeightIndex = index;
+    });
   }
 }
